c74278ef50a77988625aeeed6af490d64b4ba98b
Johannes Martinsson <johannes@antiklimax.se>
2024-10-17 21:40:07 +0200

    Add script to write out weights to json

---
+++ write-json.ts
@@ -0,0 +1,35 @@
+import { Database } from "bun:sqlite";
+
+const dbFile = "weights.db";
+
+class Weight {
+  date: string;
+  epoch: number;
+  weight: number;
+  weight_trend: number;
+}
+
+interface WeightValue {
+  date: string;
+  weight: number;
+  trend: number;
+}
+
+try {
+  using db = new Database(dbFile);
+  using query = db.query("select * from weights order by epoch;").as(Weight);
+
+  const weights: Array<WeightValue> = [];
+
+  for (const weight of query.all()) {
+    weights.push({
+      date: weight.date,
+      weight: weight.weight,
+      trend: weight.weight_trend,
+    });
+  }
+
+  Bun.write("dist/weights.json", JSON.stringify(weights));
+} catch (error) {
+  console.error("Error occurred:", error);
+}
